fecbb81186e8f0d2754e072bd4cf6258323f7042
Pranjal Pravin Deo <84141331+Pranjal-Deo@users.noreply.github.com>
2024-07-26 12:42:29 +0530

    Update app.js

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
-mongoose.connect('mongodb://localhost:27017/issueTrackerDB', { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect('mongodb+srv://Pranjal:pd04@cluster0.ouspxxw.mongodb.net/issueTrackerDB?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true });
 
 const indexRoutes = require('./routes/index');
 const projectRoutes = require('./routes/projects');
